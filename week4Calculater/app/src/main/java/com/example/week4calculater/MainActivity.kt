953971a862b105821d1d0c21ceba35543f9d8429
김대현 <gimdaehyeon@gimdaehyeon-ui-MacBookPro.local>
2022-04-07 18:02:14 +0900

    [ADD] upgrade calculater

--- week4Calculater/app/src/main/java/com/example/week4calculater/MainActivity.kt
+++ week4Calculater/app/src/main/java/com/example/week4calculater/MainActivity.kt
@@ -2,20 +2,13 @@ package com.example.week4calculater
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.view.inputmethod.InputBinding
 import android.widget.Toast
 import com.example.week4calculater.databinding.ActivityMainBinding
 import java.lang.NumberFormatException
 
-class MainActivity() : AppCompatActivity(), Parcelable {
+class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
-
-    constructor(parcel: Parcel) : this() {
-
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         var num1:Int
         var num2:Int
@@ -31,8 +24,8 @@ class MainActivity() : AppCompatActivity(), Parcelable {
                 result = num1 + num2
                 binding.result.text= getString(R.string.results,result.toString())
             } catch (e: NumberFormatException) {
-                Toast.makeText(applicationContext, "정수를 입력해주세요", Toast.LENGTH_SHORT).show()
-
+                Toast.makeText(applicationContext, "숫자를 입력해주세요", Toast.LENGTH_LONG).show()
+            }
         }
         binding.sub.setOnClickListener{
             try {
@@ -41,7 +34,7 @@ class MainActivity() : AppCompatActivity(), Parcelable {
                 result = num1 - num2
                 binding.result.text= getString(R.string.results,result.toString())
             } catch (e: NumberFormatException) {
-                Toast.makeText(applicationContext, "정수를 입력해주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "숫자를 입력해주세요", Toast.LENGTH_LONG).show()
             }
         }
         binding.div.setOnClickListener{
@@ -49,14 +42,14 @@ class MainActivity() : AppCompatActivity(), Parcelable {
                 num1 = binding.num1.text.toString().toInt()
                 num2 = binding.num2.text.toString().toInt()
                 if (num2 == 0) {
-                    Toast.makeText(applicationContext, "ERROR", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(applicationContext, "ERROR", Toast.LENGTH_LONG).show()
                 }
                 else {
                     result = num1 / num2
                     binding.result.text= getString(R.string.results,result.toString())
                 }
             } catch (e: NumberFormatException) {
-                Toast.makeText(applicationContext, "정수를 입력해주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "숫자를 입력해주세요", Toast.LENGTH_LONG).show()
             }
         }
         binding.mul.setOnClickListener{
@@ -66,8 +59,9 @@ class MainActivity() : AppCompatActivity(), Parcelable {
                 result = num1*num2
                 binding.result.text= getString(R.string.results,result.toString())
             } catch (e: NumberFormatException) {
-                Toast.makeText(applicationContext, "정수를 입력해주세요", Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "정수를 입력해주세요", Toast.LENGTH_LONG).show()
             }
         }
     }
 }
+
