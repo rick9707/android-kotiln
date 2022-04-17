66f57cdcf5646f92364625535a0bb97e786a0c77
김대현 <rick083808@gmail.com>
2022-04-17 18:12:24 +0900

    [done]

---
+++ week7Fragment/app/src/main/java/com/example/week7fragment/ExampleFragmentThree.kt
@@ -0,0 +1,22 @@
+package com.example.week7fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.week7fragment.databinding.ExamplefragmentthreeBinding
+import com.example.week7fragment.databinding.ExamplefragmenttwoBinding
+
+class ExampleFragmentThree : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ExamplefragmentthreeBinding.inflate(inflater, container, false).root
+    }
+}
+
+
+
