c4f5e71452f63a6ba7f82cd65e97db854afd52a5
김대현 <rick083808@gmail.com>
2022-05-10 19:21:55 +0900

    [ADD] not erase

--- week92/app/src/main/java/com/example/week9_2/MainActivity.kt
+++ week92/app/src/main/java/com/example/week9_2/MainActivity.kt
@@ -4,18 +4,20 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     companion object{
         const val LINE = 1
         const val CIRCLE = 2
+        const val SQUARE = 3
         var curShape = LINE
+        internal var myShapes: MutableList<MyShape> = ArrayList()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +27,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menu?.add(0,1,0, "Draw Line")
         menu?.add(0,2,0,"Draw Circle")
+        menu?.add(0,3,0, "Draw Square")
         return super.onCreateOptionsMenu(menu)
     }
 
@@ -38,6 +41,9 @@ class MainActivity : AppCompatActivity() {
                 curShape = CIRCLE
                 return true
             }
+            3 -> {
+                curShape = SQUARE
+            }
         }
         return super.onOptionsItemSelected(item)
     }
@@ -48,37 +54,89 @@ class MainActivity : AppCompatActivity() {
         var stopX = -1f
         var stopY = -1f
         val paint = Paint()
+
+
         override fun onTouchEvent(event: MotionEvent?): Boolean {
             when (event?.action) {
                 MotionEvent.ACTION_DOWN -> {
                     startX = event.x
                     startY = event.y
                 }
-                MotionEvent.ACTION_MOVE, MotionEvent.ACTION_UP -> {
+                MotionEvent.ACTION_MOVE -> {
                     stopX = event.x
                     stopY = event.y
+                    this.invalidate()
+                }
+
+                MotionEvent.ACTION_UP -> {
+                    val shape = MyShape()
+                    shape.shapeType = curShape
+                    shape.startX = startX.toInt()
+                    shape.startY = startY.toInt()
+                    shape.stopX = stopX.toInt()
+                    shape.stopY = stopY.toInt()
+                    myShapes.add(shape)
+
                     this.invalidate()
                 }
             }
             return true
         }
 
-        override fun onDraw(canvas: Canvas?) {
+        override fun onDraw(canvas: Canvas) {
             super.onDraw(canvas)
             paint.isAntiAlias = true
             paint.strokeWidth = 5f
             paint.style = Paint.Style.STROKE
             paint.color = Color.RED
-            when(curShape){
-                LINE -> {
-                    canvas?.drawLine(startX,startY,stopX,stopY,paint)
-                }
-                CIRCLE -> {
-                    val radius = Math.sqrt(Math.pow((stopX-startX).toDouble(),2.0) +
-                        Math.pow((stopY-startY).toDouble(),2.0))
-                    canvas?.drawCircle(startX,startY,radius.toFloat(),paint)
+            for (i in myShapes.indices) { //리스트 요소 처리
+                val shape2 = myShapes[i]
+
+                when(shape2.shapeType) {
+                    LINE -> {
+                        canvas.drawLine(shape2.startX.toFloat(),
+                            shape2.startY.toFloat(), shape2.stopX.toFloat(),
+                            shape2.stopY.toFloat(),
+                            paint)
+                    }
+                    CIRCLE -> {
+                        val radius = Math.sqrt(
+                            Math.pow((shape2.stopX - shape2.startX).toDouble(), 2.0) +
+                                    Math.pow((shape2.stopY - shape2.startY).toDouble(), 2.0)
+                        )
+                        canvas.drawCircle(shape2.startX.toFloat(), shape2.startY.toFloat(), radius.toFloat(), paint)
+                    }
+                    SQUARE -> {
+                        canvas.drawRect(shape2.startX.toFloat(), shape2.startY.toFloat(), shape2.stopX.toFloat(), shape2.stopY.toFloat(), paint)
+                    }
                 }
             }
+
+
+            when(curShape) {
+                LINE -> {
+                    canvas?.drawLine(startX, startY, stopX, stopY, paint)
+                }
+                CIRCLE -> {
+                    val radius = Math.sqrt(
+                        Math.pow((stopX - startX).toDouble(), 2.0) +
+                                Math.pow((stopY - startY).toDouble(), 2.0)
+                    )
+                    canvas?.drawCircle(startX, startY, radius.toFloat(), paint)
+                }
+                SQUARE -> {
+                    canvas?.drawRect(startX, startY, stopX, stopY, paint)
         }
-    }
+    }}}
+
+}
+
+class MyShape {
+
+
+    var shapeType = 0
+    var startX = 0
+    var startY: Int = 0
+    var stopX: Int = 0
+    var stopY: Int = 0
 }
